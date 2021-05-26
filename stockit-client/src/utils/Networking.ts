d4d2187a4f001dc0cb3db5ed382f6800e313949e
imnotmoon <imnotmoon@gmail.com>
2021-05-26 23:33:57 +0900

    networking module set

---
+++ stockit-client/src/utils/Networking.ts
@@ -0,0 +1,13 @@
+const IP_ADDRESS = "";
+const PORT = "";
+
+export const SOCKET = {
+	open_connection: () => {},
+	close_connection: () => {},
+	send: () => {},
+};
+
+export const API = {
+	get: () => {},
+	post: () => {},
+};
