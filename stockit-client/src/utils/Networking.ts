1493cd1dcae53261c8e1f49d2fd31e92c76b337a
imnotmoon <imnotmoon@gmail.com>
2021-08-22 23:57:57 +0900

    [fix] withCredentials 헤더 추가

--- stockit-client/src/utils/Networking.ts
+++ stockit-client/src/utils/Networking.ts
@@ -5,7 +5,7 @@ const PORT = "8080";
 
 export const REST_API_LOG = {
 	logIn: async (data: { email: string; password: string }) => {
-		const result = await axios.post(`${IP_ADDRESS}:${PORT}/api/members/login`, data, { headers: {"Content-Type": "application/json"}});
+		const result = await axios.post(`${IP_ADDRESS}:${PORT}/api/members/login`, data, { headers: {"Content-Type": "application/json", withCredentials: true }});
 		return result.data;
 	},
 	signUp: async (data: { name: string; password: string; email: string; nickname: string }) => {
@@ -33,7 +33,7 @@ export const REST_STOCK = {
 		return result;
 	},
 	all : async (token: string) => {
-		const result = await axios.get(`${IP_ADDRESS}:${PORT}/api/stocks/`, { headers: {"Authorization" : `Bearer ${token}`}});
+		const result = await axios.get(`${IP_ADDRESS}:${PORT}/api/stocks/`, { headers: {"Authorization" : `Bearer ${token}`, withCredentials: true}});
 		return result;
 	}
 }
